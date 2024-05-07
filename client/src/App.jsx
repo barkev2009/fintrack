5c2c3f9de40ba763e54ebf8d03dc4ab68b1e9c30
Greg Kovshov <barkev2009@bk.ru>
2024-05-07 08:28:19 +0700

    add public files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   const [file, setFile] = useState('');
   const [src, setSrc] = useState(null);
   const [value, setValue] = useState('');
-  const [scale, setScale] = useState(1);
   const [date, setDate] = useState(new Date());
   const id = useId();
   const log = useRef(null);
@@ -31,14 +30,16 @@ function App() {
 
           svg.current.style.transform = `scale(${window.screen.height / img.current.height})`;
           img.current.style.transform = `scale(${window.screen.height / img.current.height})`;
-          setScale(window.screen.height / img.current.height);
         }
       }, 10);
     }, [src]
   );
 
   const buttonHandler = async () => {
-    T.recognize(file, 'rus+eng', {
+    T.createWorker(['eng', 'rus'], 1, {
+      corePath: './tessdata/tesseract-core-simd.wasm.js',
+      workerPath: './tessdata/worker.js',
+      langPath: './tessdata/lang',
       logger: data => {
         log.current.innerHTML = '';
         const statusText = document.createTextNode(data.status);
@@ -47,11 +48,12 @@ function App() {
         progress.value = data.progress;
         log.current.appendChild(statusText);
         log.current.appendChild(progress);
-      },
-    })
-      .then(({ data }) => {
+      }
+    }).then(
+      async worker => {
+        const { data } = await worker.recognize(file);
         console.log(data);
-        let text = data.text;
+        // let text = data.text;
         // log.current.innerHTML = '';
         log.current.innerHTML = data.hocr;
         document.querySelectorAll('.ocr_line').forEach(item => item.outerHTML = item.outerHTML.replace('<span', '<div'));
@@ -75,7 +77,7 @@ function App() {
             }
           )
         );
-        text = text.replace(/\n\s*\n/g, '\n');
+        // text = text.replace(/\n\s*\n/g, '\n');
         const pre = document.createElement('pre');
         // pre.innerHTML = text;
         log.current.appendChild(pre);
@@ -85,7 +87,7 @@ function App() {
           createDate: date,
           payload: {
             text: data.text,
-            words: data.words.map(({bbox, language, choices, confidence, text}) => ({bbox, language, choices, confidence, text})),
+            words: data.words.map(({ bbox, language, choices, confidence, text }) => ({ bbox, language, choices, confidence, text })),
             hocr: data.hocr
           }
         }).then(
@@ -94,11 +96,14 @@ function App() {
             console.log(resp.data);
           }
         ).catch(err => console.error(err))
-      })
+        
+        await worker.terminate();
+      }
+    )
   }
 
   const drawBoxes = (data) => {
-    const mainBox = data.blocks[0].bbox;
+    // const mainBox = data.blocks[0].bbox;
     // svg.current.style.left = `${mainBox.x0}px`;
     svg.current.style.transformOrigin = 'left top';
     // svg.current.style.transform = `scale(${scale})`;
@@ -139,7 +144,7 @@ function App() {
       <div style={{ display: 'flex' }}>
         <div ref={log} id="progress"></div>
         <div style={{ position: 'relative' }} className="container">
-          <img ref={img} src={src} style={{ transformOrigin: 'left top' }} />
+          <img ref={img} src={src} alt="ocr" style={{ transformOrigin: 'left top' }} />
           <svg ref={svg} className="bboxes" style={{ position: 'absolute', transformOrigin: 'left top', left: 0 }} ></svg>
         </div>
       </div>
